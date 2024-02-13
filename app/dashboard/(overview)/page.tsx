1b43496f3a8a1288041ca2484815560d7a424c90
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-02-13 23:45:54 +0700

    nextjs tutorial chapter 9

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,44 @@
+import { Suspense } from 'react';
+
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+ 
+export default async function Page() {
+    // const revenuePromise = fetchRevenue();
+    // const latestInvoicesPromise = fetchLatestInvoices();
+    // const cardDataPromise = fetchCardData();
+
+    // const allData = await Promise.all([
+    //   revenuePromise,
+    //   latestInvoicesPromise,
+    //   cardDataPromise
+    // ])
+
+    // const revenue = allData[0];
+    // const latestInvoices = allData[1];
+    // const cardData = allData[2];
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
